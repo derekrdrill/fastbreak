3ef0d2008138d3841a247553f2b8ca5ea304e74d
Derek Drill <derekrdrill@gmail.com>
2025-11-02 22:28:10 -0500

    ✨ updates redirect logic to disregard when local

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { getSupabaseClient } from '@/app/_helpers/db/client/client';
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
@@ -8,20 +7,8 @@ export async function GET(request: Request) {
   const tokenHash = requestUrl.searchParams.get('token_hash');
   const type = requestUrl.searchParams.get('type');
   const origin = requestUrl.origin;
-  const cookieStore = await cookies();
 
-  const supabase = createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
-      },
-      setAll(cookiesToSet: Array<{ name: string; value: string; options: CookieOptions }>) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          cookieStore.set(name, value, options);
-        });
-      },
-    },
-  });
+  const supabase = await getSupabaseClient();
 
   if (code) {
     const { data, error } = await supabase.auth.exchangeCodeForSession(code);
