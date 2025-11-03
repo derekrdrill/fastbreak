bbe9a98e9d94a3e4330563513b94b39842439794
Derek Drill <derekrdrill@gmail.com>
2025-11-03 12:02:10 -0500

    🧹 remove unused hooks

--- app/_components/_hooks/useFormControl.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useFormContext, type Control, type FieldValues } from 'react-hook-form';
-
-export function useFormControl<TFieldValues extends FieldValues>(control?: Control<TFieldValues>) {
-  const ctx = useFormContext<TFieldValues>();
-  return control ?? ctx.control;
-}
